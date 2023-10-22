e32c74c98cde7026f3f014cbb412c6305b34d20a
Sufana_Aljawi <Sufanaaljawi@gmail.com>
2023-10-22 14:21:49 +0300

    The lab is solved - Q1

--- lab_recursion.py
+++ lab_recursion.py
@@ -1,11 +1,12 @@
-def vowels_letters(string:int)->int:
-    count=0
-    vowels={'a','i','o','u','e'}
-    for i in string:
-        if i in vowels:
-            count +=1
-            return count
 
+vowels=['a','i','o','u','e']
+def vowels_letters(string:str)->int:
+    if len(string)==0:
+        return 0
+    count = 0
+    if string[0].lower() in vowels:
+        count=1
+        return count + vowels_letters(string[1:])
 
 value = input("Enter a sentence: ")
 print(vowels_letters(value))
