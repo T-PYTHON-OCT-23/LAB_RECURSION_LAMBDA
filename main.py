167376bc6913127be1ca91a961c0bfcab33d4c23
Bader alotaibi <baderalotaibi3@gmail.com>
2023-10-22 14:12:11 +0300

    remove unwanted lines

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 vowels=("a","e","i","o","u")
 def countVowels(phrase):
-    count=0
     if phrase == "":
-        return count
+        return 0
     if phrase[0].lower() in vowels:
-        count+=1
-    return count + countVowels(str(phrase[1:]))
+        return 1 + countVowels(str(phrase[1:]))
+    else:
+        return countVowels(str(phrase[1:]))
 
 print("number of vowels in your pharse",countVowels("I love python"))
-    
 list_=[40,35, 10, 15, 20]
 square=lambda x: x**2
 timesTwo=list(map(square,list_))
