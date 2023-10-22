24d0e89500e112dfe523162226aeb051582f980f
Reef <reef.zayed2@gmail.com>
2023-10-22 14:21:48 +0300

    Done Lab

---
+++ LAB_RECURSION_LAMBDA.py
@@ -0,0 +1,16 @@
+def many_vowels(phrase)->int:
+    count=0 
+    if len(phrase)==0:
+        return count
+    if phrase[0] in "aeiou":
+        count+=1
+    return count+many_vowels(phrase[1:])
+
+phrase="I love python"
+print(f"number vowels in {many_vowels(phrase.lower())}")
+
+
+
+# 2) create a new list containing each number from the previous list mutliplied by itself
+result= map(lambda n :n*n, [40,35, 10, 15, 20])
+print(list(result))
---
+++ bonus.py
@@ -0,0 +1,27 @@
+
+#use for 
+def FizzBuzz():
+    for number in range(1,100):
+        if number % 5 == 0 and number % 3 == 0 :
+            print(f"{number} is FizzBuzz")
+        elif number % 3 == 0:
+            print(f"{number} is Fizz")
+        elif number % 5 == 0:
+            print(f"{number} is Buzz")
+
+FizzBuzz()
+
+
+ # use while       
+def FizzBuzz_tow():
+    numbers=1
+    while numbers <100:
+        if numbers % 5 == 0 and numbers % 3 == 0 :
+            print(f"{numbers} is FizzBuzz")
+        elif numbers % 3 == 0:
+            print(f"{numbers} is Fizz")
+        elif numbers % 5 == 0:
+            print(f"{numbers} is Buzz")
+        numbers+=1
+
+FizzBuzz_tow()
