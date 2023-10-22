5bae9183cbbbfbe5d467bf413de84261d5bb51eb
Bader alotaibi <baderalotaibi3@gmail.com>
2023-10-22 12:55:00 +0300

    easy bouns

---
+++ bouns.py
@@ -0,0 +1,19 @@
+def fizFor():
+    for i in range(1,101):
+        temp=""
+        i%3==0
+        temp+="fizz"
+        i%5==0
+        temp+="Buzz"
+
+def fizWhile():
+    i=1
+    while i!=101:
+        temp=""
+        i%3==0
+        temp+="fizz"
+        i%5==0
+        temp+="Buzz"
+        i+=1
+        
+        
