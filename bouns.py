e757657b159e516f6579b45604c84c4f820f3a80
Bader alotaibi <baderalotaibi3@gmail.com>
2023-10-22 12:58:26 +0300

    fix bugs

--- bouns.py
+++ bouns.py
@@ -1,19 +1,23 @@
 def fizFor():
     for i in range(1,101):
         temp=""
-        i%3==0
-        temp+="fizz"
-        i%5==0
-        temp+="Buzz"
+        if i%3==0:
+            temp+="fizz"
+        if i%5==0:
+            temp+="Buzz"
+        if temp!="":
+            print(temp)
 
 def fizWhile():
     i=1
     while i!=101:
         temp=""
-        i%3==0
-        temp+="fizz"
-        i%5==0
-        temp+="Buzz"
+        if i%3==0:
+            temp+="fizz"
+        if i%5==0:
+            temp+="Buzz"
         i+=1
+        if temp!="":
+            print(temp)
         
         
